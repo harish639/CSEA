35246c7a6e14f8879a5edd9729b5d7d251055d12
harish639 <133838741+harish639@users.noreply.github.com>
2023-05-17 10:34:32 +0530

    Add files via upload

---
+++ volsu.py
@@ -0,0 +1,6 @@
+r=int(input("enter a value"))
+h=int(input("enter a value"))
+volume=3.14*r*r*h
+saofcy=((2*3.14*r)*h)+((3.14*r**2)*2)
+print('value of volume :',volume)
+print('surface area of a cylinder is :',saofcy)

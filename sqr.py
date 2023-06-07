6c98f1ff4c96290f5a13de247da7a12a31546333
harish639 <133838741+harish639@users.noreply.github.com>
2023-06-07 09:56:09 +0530

    Add files via upload

---
+++ sqr.py
@@ -0,0 +1,7 @@
+a=int(input("enter a number:"))
+b=int(input("enter other number:"))
+c=int(input("enter third number:"))
+if(a**2+b**2==c**2):
+    print("1")                      
+else:
+    print("0")
